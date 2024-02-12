bb1ee674db02e28f25f59d87ae6b20c7a6c7d96f
BasmaMounir <basmamounir57@gimal.com>
2024-02-12 20:40:41 +0200

    finished Tasbih tab and Radio tab

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:islamic_app/Home/Hadith/HadithDetails.dart';
 import 'package:islamic_app/Home/HomeScreen.dart';
 import 'package:islamic_app/Home/Quran/SuraDetails.dart';
+import 'package:islamic_app/SplashScreen.dart';
 import 'package:islamic_app/my_theme.dart';
 
 void main() {
@@ -13,11 +14,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: HomeScreen.routeName,
+      initialRoute: SplashScreen.routeName,
       routes: {
-        HomeScreen.routeName: (context) => HomeScreen(),
-        SuraDetails.routeName: (context) => SuraDetails(),
-        HadithDetails.routeName: (context) => HadithDetails(),
+        SplashScreen.routeName: (_) => SplashScreen(),
+        HomeScreen.routeName: (_) => HomeScreen(),
+        SuraDetails.routeName: (_) => SuraDetails(),
+        HadithDetails.routeName: (_) => HadithDetails(),
       },
       theme: myTheme.lightTheme,
     );
