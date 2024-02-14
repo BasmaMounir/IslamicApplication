2d02ce53823b72b8488f452c3b3a7dd99d360fd3
Basma Mounir <basmamounir57@gmail.com>
2024-02-14 21:47:12 +0200

    Revert "Feature home screen"

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:islamic_app/Home/Hadith/HadithDetails.dart';
-import 'package:islamic_app/Home/HomeScreen.dart';
-import 'package:islamic_app/Home/Quran/SuraDetails.dart';
-import 'package:islamic_app/SplashScreen.dart';
-import 'package:islamic_app/my_theme.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,16 +7,6 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: SplashScreen.routeName,
-      routes: {
-        SplashScreen.routeName: (_) => SplashScreen(),
-        HomeScreen.routeName: (_) => HomeScreen(),
-        SuraDetails.routeName: (_) => SuraDetails(),
-        HadithDetails.routeName: (_) => HadithDetails(),
-      },
-      theme: myTheme.lightTheme,
-    );
+    return MaterialApp();
   }
 }
